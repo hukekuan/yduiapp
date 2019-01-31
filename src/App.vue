674a3c1e4804a13fec2ee793bf67309bc8f9af4b
hukekuan@163.com <hukekuan@163.com>
2019-01-31 23:37:09 +0800

    新增ydui样式

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,109 @@
+<!--<template>-->
+  <!--<div id='app'>-->
+    <!--<img src='./assets/logo.png'>-->
+    <!--<router-view/>-->
+  <!--</div>-->
+<!--</template>-->
+
+具体使用示例请参照对照右侧代码。
+
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
-  </div>
+  <yd-button-group>
+    <yd-button @click.native="openConfrim" size="large">Confirm</yd-button>
+    <yd-button @click.native="openCustomConfrim" size="large">Confirm->Custom</yd-button>
+    <yd-button @click.native="openAlert" size="large" type="hollow">Alert</yd-button>
+    <yd-button @click.native="toastSuccess" size="large" type="danger">Toast->Success</yd-button>
+    <yd-button @click.native="toastError" size="large" type="danger">Toast->Error</yd-button>
+    <yd-button @click.native="toastNone" size="large" type="danger">Toast->None</yd-button>
+    <yd-button @click.native="openLoading" size="large" type="hollow">Loading</yd-button>
+    <yd-button @click.native="openNotify" size="large" type="hollow">Notify</yd-button>
+  </yd-button-group>
 </template>
 
-<script>
+<script type="text/babel">
 export default {
-  name: 'App'
-}
-</script>
+    methods: {
+      openConfrim () {
+        this.$dialog.confirm({
+          title: '选填标题',
+          mes: '我有一个小毛驴我从来也不骑！',
+          opts: () => {
+            this.$dialog.toast({mes: '你点了确定', timeout: 1000});
+          }
+        })
+      },
+      openCustomConfrim() {
+        this.$dialog.confirm({
+          title: '选填标题',
+          mes: '我有一个小毛驴我从来也不骑！',
+          opts: [
+            {
+              txt: '取消',
+              color: false,
+              callback: () => {
+                this.$dialog.toast({mes: '你点了取消', timeout: 1000});
+              }
+            },
+            {
+              txt: '犹豫一下',
+              stay: true,
+              color: '#CCC',
+              callback: () => {
+                this.$dialog.toast({mes: '别犹豫了', timeout: 1000});
+              }
+            },
+            {
+              txt: '确定',
+              color: true,
+              callback: () => {
+                this.$dialog.toast({mes: '你点了确定', timeout: 1000});
+              }
+            }
+          ]
+        });
+      },
+      openAlert() {
+        this.$dialog.alert({mes: '消息一出，休想滚动屏幕[移动终端]！'});
+      },
+      toastSuccess() {
+        this.$dialog.toast({
+          mes: '保存成功',
+          timeout: 1500,
+          icon: 'success'
+        })
+      },
+      toastError() {
+        this.$dialog.toast({
+          mes: '保存失败',
+          timeout: 1500,
+          icon: 'error',
+          callback: () => {
+            this.$dialog.alert({mes: '给你一次重来的机会！'})
+          }
+        });
+      },
+      toastNone() {
+        this.$dialog.toast({
+          mes: '一个没有任何图标的提示！',
+          timeout: 1500
+        });
+      },
+      openLoading () {
+        this.$dialog.loading.open('很快加载好了')
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+        setTimeout (() => {
+          this.$dialog.loading.close()
+        }, 2000);
+      },
+      openNotify () {
+        this.$dialog.notify({
+          mes: '5秒后自动消失，点我也可以消失！',
+          timeout: 5000,
+          callback: () => {
+            console.log('我走咯！')
+          }
+        });
+      }
+    }
+  }
+</script>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,52 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+// import YDUI from 'vue-ydui'
+// import 'vue-ydui/dist/ydui.rem.css'
+// import 'vue-ydui/dist/ydui.base.css'
+import 'vue-ydui/dist/ydui.base.css'
+import {ActionSheet} from 'vue-ydui/dist/lib.rem/actionsheet'
+import {Button, ButtonGroup} from 'vue-ydui/dist/lib.rem/button'
+import {CellGroup, CellItem} from 'vue-ydui/dist/lib.rem/cell'
+import {Icons} from 'vue-ydui/dist/lib.rem/icons'
+import {ListTheme, ListItem, ListOther} from 'vue-ydui/dist/lib.rem/list'
+import {GridsGroup, GridsItem} from 'vue-ydui/dist/lib.rem/grids'
+import {TabBar, TabBarItem} from 'vue-ydui/dist/lib.rem/tabbar'
+import {Badge} from 'vue-ydui/dist/lib.rem/badge'
+import {Layout} from 'vue-ydui/dist/lib.rem/layout'
+import {Slider, SliderItem} from 'vue-ydui/dist/lib.rem/slider'
+import { Confirm, Alert, Toast, Notify, Loading } from 'vue-ydui/dist/lib.rem/dialog'
+
 import App from './App'
 import router from './router'
 
 Vue.config.productionTip = false
+
+// Vue.use(YDUI)
+Vue.component(ActionSheet.name, ActionSheet)
+Vue.component(Button.name, Button)
+Vue.component(ButtonGroup.name, ButtonGroup)
+Vue.component(CellGroup.name, CellGroup)
+Vue.component(CellItem.name, CellItem)
+Vue.component(Icons.name, Icons)
+Vue.component(ListTheme.name, ListTheme)
+Vue.component(ListItem.name, ListItem)
+Vue.component(ListOther.name, ListOther)
+Vue.component(GridsGroup.name, GridsGroup)
+Vue.component(GridsItem.name, GridsItem)
+Vue.component(TabBar.name, TabBar)
+Vue.component(TabBarItem.name, TabBarItem)
+Vue.component(Badge.name, Badge)
+Vue.component(Layout.name, Layout)
+Vue.component(Slider.name, Slider)
+Vue.component(SliderItem.name, SliderItem)
+Vue.prototype.$dialog = {
+  confirm: Confirm,
+  alert: Alert,
+  toast: Toast,
+  notify: Notify,
+  loading: Loading,
+}
 
 /* eslint-disable no-new */
 new Vue({
